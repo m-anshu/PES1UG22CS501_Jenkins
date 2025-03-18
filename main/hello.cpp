d913f0d8802309ee546ad5bac4e54ab63149cc7a
Sai Mohananshu <mohananshuj@gmail.com>
2025-03-18 16:42:22 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << "PES1UG22CS501 modified for output\n";
+  cout << "PES1UG22CS501 modified\n";
   return 0;
 }
